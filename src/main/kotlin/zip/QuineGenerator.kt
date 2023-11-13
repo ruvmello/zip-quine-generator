6997ecb6547fffb744ee84188262fa7c66141233
ruvmello <ruben.vanmello@gmail.com>
2023-11-13 18:54:00 +0100

    Further quine remarks and improvements

--- src/main/kotlin/zip/QuineGenerator.kt
+++ src/main/kotlin/zip/QuineGenerator.kt
@@ -3,6 +3,7 @@ package zip
 import lz77.LZ77Literal
 import lz77.LZ77Repeat
 import lz77.LZ77Token
+import utils.distanceMapStaticHuffman
 import utils.getByteArrayOf2Bytes
 import java.io.File
 
@@ -19,12 +20,19 @@ class QuineGenerator {
         // Note: Header of a literal block is 5 bytes, so L1 is 5 bytes
         getLiteralWithSize(output.size + 5).forEach { output.add(LZ77Literal(it.toUByte())) }
 
-        // Rp+1, TODO: make this fit in byte boundary
-        val p_and_1 = output.size
-        for (i in 1..(p_and_1 / 258)) {
-            output.add(LZ77Repeat(p_and_1, 258))
+        // Rp+1
+        val pAnd1 = output.size
+
+        // Get number of bits, distance is always 5 + extraBits
+        var totalBitsForRepeat = 0
+        val base = distanceMapStaticHuffman.keys.findLast { pAnd1 >= it }
+        val extraBits = distanceMapStaticHuffman[base]!!.second
+        for (i in 1..(pAnd1 / 258)) {
+            output.add(LZ77Repeat(pAnd1, 258))
+            totalBitsForRepeat += (5 + extraBits) + 8 // (distance) + bits used for 258 as length = (5 + extraBits) + 8
         }
-        output.add(LZ77Repeat(p_and_1, p_and_1 % 258))
+        // TODO: make this fit in byte boundary, maybe use the last repeat as well, because of the last repeat has length 1 ...
+        output.add(LZ77Repeat(pAnd1, pAnd1 % 258))
 
         // Lx
 
@@ -34,5 +42,10 @@ class QuineGenerator {
     fun getLiteralWithSize(size: Int): ByteArray {
         return byteArrayOf(0.toByte()) + getByteArrayOf2Bytes(size) +
                 getByteArrayOf2Bytes(size.inv())
+    }
+
+    // Split the last repeat so that it ends on a byte boundary
+    fun getRepeatsUntilByteBoundary() {
+
     }
 }
